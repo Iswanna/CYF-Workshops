5f161c7f585fd9c7f9bc5def0ee791dbb7f8d25f
iswat <ibeetek@gmail.com>
2025-11-12 12:15:17 +0000

    Comment out method 2 of using for...of loop to get the sum of an array

--- debugging/bank/sumArray.js
+++ debugging/bank/sumArray.js
@@ -6,3 +6,13 @@ const sumArray = (numbers) => {
   return total;
 };
 console.log(sumArray([1, 2, 3]));
+
+ // Method 2: using for of loop 
+// const sumArray = (numbers) => {
+//   let total = 0;
+//   for (let n of numbers) {
+//     total += n;
+//   }
+//   return total;
+// };
+// console.log(sumArray([1, 2, 3]));
